7ecf8f89ce1b242953d61685b6470d78e12fa145
hond64 <wentzelmiguel@gmail.com>
2026-04-13 12:17:59 +0000

    Changes made

--- GenericBinary.java
+++ GenericBinary.java
@@ -1,1 +1,11 @@
+class BinaryTreeNode<E>{
+    E value;
+    BinaryTreeNode<E>  left;
+    BinaryTreeNode<E> Right;
 
+}
+
+public class BinaryTree<E>{
+    int size;
+    BinaryTreeNode<E> root;
+}
